e1a53938f1482f43dd71d5327c9e862eccd4429b
arsenelanang <arsenelanang@gmail.com>
2016-11-01 12:52:32 +0800

    g
    coba

---
+++ balianse/routes/web.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+
+
+
+Route::get('/', 'PagesController@home');
+Route::get('/contact','PagesController@contact');
+Route::get('/about', 'PagesController@about');
+Route::get('/contact', 'TicketsController@create');
+Route::post('/contact', 'TicketsController@store');
+Route::get('/tickets', 'TicketsController@index');
+Route::get('/ticket/{slug?}', 'TicketsController@show');
+Route::get('/ticket/{slug?}/edit','TicketsController@edit');
+Route::post('/ticket/{slug?}/edit','TicketsController@update');
+Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+Route::get('sendemail', function () {
+
+    $data = array(
+        'name' => "Learning Laravel",
+    );
+
+    Mail::send('emails.welcome', $data, function ($message) {
+
+        $message->from('trisnasumantara@gmail.com', 'Learning Laravel');
+
+        $message->to('arsenelanang@gmail.com')->subject('Learning Laravel test email');
+
+    });
+
+    return "Your email has been sent successfully";
+
+});
+
+Route::post('/comment', 'CommentsController@newComment');
